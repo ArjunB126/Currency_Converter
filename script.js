ec4e4d42becf28fe544589d2f7664bbf46b173e2
ArjunB126 <arjunbajare86@gmail.com>
2024-02-04 23:22:13 +0530

    Updates Loops in Js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const btn = document.querySelector("#btn");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
+
+// Loop through each dropdown element
+for (let select of dropdown)
+{
+    for(currCode in countryList) // Loop through each currency code in the countryList object
+    {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value= currCode;
+
+}
