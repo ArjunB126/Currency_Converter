5d815d20d540a6f033476419a6b1b01fa8343508
ArjunB126 <arjunbajare86@gmail.com>
2024-02-04 23:17:53 +0530

    Update the base URL for the currency API and add a dropdown menu for currency selection

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdown = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("#btn");
